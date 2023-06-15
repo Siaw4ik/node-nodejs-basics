deb73001a1afe2538bbf75eed66581ca843510de
Siaw4ik <peretsviach@gmail.com>
2023-06-15 18:22:00 +0200

    feat: create.js - completed the task of creating the file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,29 @@
+import fs from 'node:fs/promises'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const moduleURL = import.meta.url;
+  const __filename = fileURLToPath(moduleURL);
+  const __dirname = dirname(__filename);
+
+  const content = 'I am fresh and young';
+  const filePath = join(__dirname, 'files', 'fresh.txt');
+
+  try{
+    const fileExists = await fs.access(filePath)
+      .then(() => true)
+      .catch(() => false)
+
+    if(fileExists){
+      throw new Error('FS operation failed');
+    } 
+
+    await fs.writeFile(filePath, content);
+  }
+  catch(error){
+    console.log(error.message)
+  }
 };
 
 await create();
