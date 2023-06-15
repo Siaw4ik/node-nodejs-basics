3495660c8721aef22f54ddb66008250ffdf341f1
Siaw4ik <peretsviach@gmail.com>
2023-06-15 18:55:36 +0200

    feat: delete.js - delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,28 @@
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const remove = async () => {
-    // Write your code here 
+  const moduleURL = import.meta.url;
+  const __filename = fileURLToPath(moduleURL);
+  const __dirname = dirname(__filename);
+
+  try{
+    const filePath = join(__dirname, 'files', 'fileToRemove.txt')
+
+    const isFile = await fs.access(filePath)
+    .then(() => true)
+    .catch(() => false)
+
+    if(!isFile) {
+      throw new Error('FS operation failed');
+    }
+
+    await fs.unlink(filePath);
+  }
+  catch(error){
+    console.log(error.message);
+  }
 };
 
 await remove();
