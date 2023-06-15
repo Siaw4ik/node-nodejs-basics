0a31ab2902020bb26d529fbb47eb2fd563f1dd28
Siaw4ik <peretsviach@gmail.com>
2023-06-15 21:55:44 +0200

    feat: env.js - print env veriables in console

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const arr = [];
+  for (const [name, value] of Object.entries(process.env)) {
+    if (name.startsWith(prefix)) {
+      const variableName = name.slice(prefix.length);
+      arr.push(`${name}=${value}`)
+    }
+  }
+
+  console.log(arr.reverse().join('; '))
 };
 
 parseEnv();
