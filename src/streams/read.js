29925718fb9ef1064fe0da3989f4793ff855f427
Siaw4ik <peretsviach@gmail.com>
2023-06-16 14:37:01 +0200

    feat: read.js - read file fileToRead.txt content using Readable Stream and print it's content into process.stdout

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream/promises'
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  
+  const filePath = join(__dirname, 'files', 'fileToRead.txt')
+  const fileStream = fs.createReadStream(filePath);
+
+  await pipeline(fileStream, process.stdout)
 };
 
 await read();
