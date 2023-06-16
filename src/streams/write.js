2d530f8dba60f3a30d04de5a2a5118bd8cc09c90
Siaw4ik <peretsviach@gmail.com>
2023-06-16 15:06:05 +0200

    feat: write.js - write process.stdin data into file fileToWrite.txt content using Writable Stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream/promises'
+
 const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt')
+  const fileStream = fs.createWriteStream(filePath, {flags: 'a'});
+
+  await pipeline(process.stdin, fileStream)
 };
 
 await write();
