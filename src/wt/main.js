7a2141d5874aa6340c2d920fa0eaad768e651f70
Siaw4ik <peretsviach@gmail.com>
2023-06-16 19:16:29 +0200

    feat: Worker Threads - implement function that creates number of worker threads (equal to the number of host machine logical CPU cores) from file worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import {cpus} from 'os';
+import { Worker, workerData } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const workerPath = join(__dirname, 'worker.js')
+
+  const culculateFibonacci = (workerData) => new Promise((resolve) => {
+    const worker = new Worker(workerPath, {workerData})
+
+    worker.on('message', (data) => resolve({status: 'resolved', data}));
+    worker.on('error', () => resolve({status: 'error', data: null}));
+  })
+
+  const calculation = new Array(cpus().length).fill(null).map((value, index) => culculateFibonacci(index + 10))
+  const data = await Promise.all(calculation);
+
+  console.log(data);
+
+
+
 };
 
 await performCalculations();
