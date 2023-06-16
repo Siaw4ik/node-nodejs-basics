1a10d837f3c37ed46ab7a1275598cdf6477a3ac3
Siaw4ik <peretsviach@gmail.com>
2023-06-16 12:47:39 +0200

    feat: calcHash.js - calculate SHA256 hash for file fileToCalculateHashFor.txt and log it into console as hex

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import { createHash } from 'node:crypto'
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const moduleURL = import.meta.url;
+  const __filename = fileURLToPath(moduleURL);
+  const __dirname = dirname(__filename);
+  
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+  const content = await fs.readFile(filePath)
+
+  const hash = await createHash('sha256').update(content).digest('hex')
+
+  console.log(hash)
+
 };
 
 await calculateHash();
